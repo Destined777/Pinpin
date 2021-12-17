29b564c58a3b78201cc6398e6a21cd41aeebb82b
Destined777 <695693802@qq.com>
2021-12-17 08:43:08 +0800

    little change

--- consts/consts.go
+++ consts/consts.go
@@ -5,7 +5,7 @@ const (
 	//Verify code is vaild in 24h
 	VERIFYCODE_VALID_TIME = 24 * 60 * 60
 
-	REDIS_VERIFY_CODE_SUFFIX = "verify_code:"
+	REDIS_VERIFY_CODE_SUFFIX = "verifyCode:"
 
 	TOKEN_SCRECT_KEY = "tokenemail"
 )
