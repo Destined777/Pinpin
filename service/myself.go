6e1fdde271cfb09dca4195f344f96cbe4db921bc
Destined777 <695693802@qq.com>
2021-12-26 13:00:24 +0800

    fix:myself

--- service/myself.go
+++ service/myself.go
@@ -33,6 +33,8 @@ func GetMyPinpinService(email string) (results []map[string]interface{}, err err
 			"IsDeleted":                value.IsDeleted,
 			"Owner_email":              value.Owner_email,
 			"ReplyNum":                 value.ReplyNum,
+			"Created_at":				value.CreatedAt,
+			"Updated_at":				value.UpdatedAt,
 			//用户是否收藏该拼拼帖
 			"IsFollow": isfollow,
 		})
